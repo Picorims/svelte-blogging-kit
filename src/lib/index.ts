13925a64a0f90f97480e27c46c596af3b116d496
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-10-26 00:42:15 +0200

    ✨ feat(project): barebone post structure

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // Reexport your entry components here
+
+import BlogPage, { type Config } from "./components/BlogPage.svelte";
+
+export {BlogPage, type Config};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,90 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	/*
+    MIT License
+    
+    Copyright (c) 2024 Charly Schmidt aka Picorims<picorims.contact@gmail.com>
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    */
+
+	import BlogPage, { type Config } from '$lib/components/BlogPage.svelte';
+
+	let config: Config = {
+		title: 'Svelte Blogging Kit'
+	};
+</script>
+
+<BlogPage postTitle="Presentation - static pages for blogging" date="26 October 2024" {config} {postContent} />
+
+{#snippet postContent()}
+    <h2>Posts are plain HTML with a bit of Svelte</h2>
+    <p>
+        While the BlogPage component renders the page layout and handle the config and metadata, the content is just an HTML snippet.
+        This means that headers are written using <code>&lt;h2&gt;</code>, <code>&lt;h3&gt;</code>, etc. tags,
+        and paragraphs are written using <code>&lt;p&gt;</code> tags, for example.
+    </p>
+
+    <h2>The theme is customizable</h2>
+    <p>
+        A default barebones theme is provided, but you can create your own theme. To do so, define a new class somewhere in your project,
+        named after the theme you want to create, and pass it to the config. Then, style the elements of the page as you see fit.
+        They are all prefixed with <code>__sbk__</code> to avoid conflicts with other styles. Take advantage of the devtools
+        to see the classes applied to each element.
+    </p>
+
+    <h2>Getting started</h2>
+    <p>
+        View the source code of this page in <code>src/routes/+page.svelte</code> to see how this post was created!
+        You can also check the <code>BlogPage</code> component in <code>src/lib/components/BlogPage.svelte</code> to see how the default theme is defined.
+    </p>
+    <p>
+        <a href="https://github.com/picorims/svelte-blogging-kit">See the GitHub repository here.</a>
+    </p>
+
+    <h2>What you can put in your post</h2>
+    
+    <h3>Classic HTML</h3>
+    <p>
+        Create headers with <code>&lt;h2&gt;</code>, <code>&lt;h3&gt;</code>, etc. tags (<code>&lt;h1&gt;</code> is for the post title!),
+        and paragraphs with <code>&lt;p&gt;</code> You can also use your own components if you want, although it is prefered to keep it simple.
+        Theming of the post is based on the html tags or classes that you use.
+    </p>
+
+    <h3>Additional shipped components</h3>
+    <p>
+        Some additional quality of life components are provided to make blog writing easier.
+    </p>
+
+    <h4>Image</h4>
+    <p>
+        The <code>Image</code> component allows you to easily add images to your post. It takes a <code>src</code> prop for the image source,
+        and an <code>alt</code> prop for the alt text. You can also pass a <code>caption</code> prop to add a caption below the image.
+    </p>
+
+    <h4>FileAttachement</h4>
+    <p>
+        The <code>FileAttachement</code> component allows you to attach files to your post that the reader can download.
+    </p>
+
+    <h4>Audio</h4>
+    <p>
+        The <code>Audio</code> component allows you to embed audio files in your post. It uses the native audio player
+        with controls enabled by default.
+    </p>
+{/snippet}
