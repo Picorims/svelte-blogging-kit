e2b58db83c33ad734be48a37998d4803e18051b8
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-10-26 14:32:26 +0200

    🐞 fix(demo): fix links, again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -101,9 +101,9 @@
         The <code>FileAttachement</code> component allows you to attach files to your post that the reader can download.
     </p>
 
-    <FileAttachment src={base + audioExample} />
-    <FileAttachment src={base + imageExample} />
-    <FileAttachment src={base + txtFileExample} />
+    <FileAttachment src={audioExample} />
+    <FileAttachment src={imageExample} />
+    <FileAttachment src={txtFileExample} />
 
     <h4>Audio</h4>
     <p>
